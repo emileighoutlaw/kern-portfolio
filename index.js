466646c638329d772e7a2687da4bd6ce5f6d1063
Emileigh Barnes <emily.barnes@gsa.gov>
2015-09-01 23:16:54 -0400

    Adds JS

--- index.js
+++ index.js
@@ -1,39 +1,52 @@
 
 $(document).ready(function() {
 
-		var win = $(window);
-		var window_height = win.innerHeight();
+	    $(".poemLink").click(function(){
+					event.preventDefault();
+					$(".poem").show();
+					console.log("hi");
+				});
 
-		$('.nav-a-thingy').hide();
+	    });
 
-		win.resize(function() {
-			win = $(window);
-			window_height = win.innerHeight();
-		});
 
-		function isScollLessThanWindowHeight() {
-			return win.scrollTop() > window_height;
-		}
 
-		win.scroll(function() {
-			(isScollLessThanWindowHeight())
-					? $('.nav-a-thingy').show()
-					: $('.nav-a-thingy').hide();
-		});
 
-	});
 
-  // $(".main").onepage_scroll();
-  //
-  //
-  // $(window).scroll(function() {
-  //   if ($(window).scrollTop() > 100) {
-  //     //show navigation
-  //     //do an animation to slide down
-  //   } else {
-  //
-  //     //hide my navigation
-  //     //do animation to slide up
-  //
-  //   }
-  // });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		// Old sticky nav-a-thingy
+		// var win = $(window);
+		// var window_height = win.innerHeight();
+		//
+		// $('.nav-a-thingy').hide();
+		//
+		// win.resize(function() {
+		// 	win = $(window);
+		// 	window_height = win.innerHeight();
+		// });
+		//
+		// function isScollLessThanWindowHeight() {
+		// 	return win.scrollTop() > window_height;
+		// }
+		//
+		// win.scroll(function() {
+		// 	(isScollLessThanWindowHeight())
+		// 			? $('.nav-a-thingy').show()
+		// 			: $('.nav-a-thingy').hide();
+		// });
+		//
+		// });
